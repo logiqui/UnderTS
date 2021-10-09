52bad02269b52b03e4581946376a43e02582a491
logiqui <63269267+logiqui@users.noreply.github.com>
2021-10-08 21:55:43 -0300

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -16,9 +16,10 @@ export default class Ready extends Event {
     box([
       `Eu sou ${chalk.yellow(this.client.user?.tag)} e fui iniciado com o ID: ${chalk.yellow(this.client.user?.id)}`,
       `O Bot foi iniciado com ${chalk.yellow(this.client.users.cache.size)} usuarios em ${chalk.yellow(this.client.guilds.cache.size)} servidores`,
-      `Foram carregados um total de ${chalk.yellow(this.client.commands.size)} comandos.`,
+      `Foram carregados um total de ${chalk.yellow(this.client.commands.size)} comandos.`
     ])
 
+    this.client.user?.setActivity({ name: `em Under Evoluion`, type: 'PLAYING' })
     this.client.registryCommands()
   }
 }
